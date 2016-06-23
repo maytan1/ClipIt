85532f94c941c8ed35679dab0e1837b7d2c9b563
maytan <maytan@Maytan-PC>
2016-06-23 11:23:30 +0530

    Pre-Release

--- src/main/java/com/simpsolution/clipit/MainActivity.java
+++ src/main/java/com/simpsolution/clipit/MainActivity.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 SimpSolutions.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.simpsolution.clipit;
 
 import android.annotation.TargetApi;
@@ -52,6 +75,7 @@ import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedExceptio
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import org.florescu.android.rangeseekbar.RangeSeekBar;
 import org.florescu.android.rangeseekbar.RangeSeekBar.OnRangeSeekBarChangeListener;
@@ -488,7 +512,7 @@ public class MainActivity extends AppCompatActivity {
     //<editor-fold defaultstate="collapsed" desc="Execute Function">
     public void execute(View view){
         if(selectedImagePath != null && selectedImagePath.length() > 0){
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US);
             String postName = sdf.format(new Date());
             String cmd;
             
